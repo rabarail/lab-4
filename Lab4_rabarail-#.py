bf994db24136eaede8fef82c9bb194a2b5647966
Rajani <rajani.kami004@gmail.com>
2026-02-09 14:39:12 -0500

    Add Lab 4 file

---
+++ Lab4_rabarail-#.py
@@ -0,0 +1,5 @@
+"""Program name: Guess the number - Hangman Style
+AUthor: Rajani Baraili
+Purpose: guess random number between 1 and 15. Each guess reveals part of hangman drawing.
+Starter code: None
+Date:02/09/2026"""
